651937141abacdc666eb55a4abfa65855a4dd1c2
Cem Telliagaoglu <cem.telliagaoglu@gmail.com>
2023-08-07 15:39:03 +0300

    convert charactersResponse to viewModel and present

--- RickNMorty/Home/HomePresenter.swift
+++ RickNMorty/Home/HomePresenter.swift
@@ -8,11 +8,25 @@
 import Foundation
 
 protocol HomePresentationLogic: AnyObject {
-    
+    func presentCharacters(model: Home.Case.Response)
+    func presentError(message: String)
 }
 
 final class HomePresenter: HomePresentationLogic {
     
     weak var viewController: HomeDisplayLogic?
-    
+    private var charactersViewModel = [Home.Case.ViewModel]()
+
+    func presentCharacters(model: Home.Case.Response) {
+        let allCharacters = model.results
+        allCharacters.forEach {
+            charactersViewModel.append(.init( name: $0.name,
+                                              imageURLString: $0.image))
+        }
+        viewController?.displayCharacters(viewModels: charactersViewModel)
+    }
+
+    func presentError(message: String) {
+
+    }
 }
