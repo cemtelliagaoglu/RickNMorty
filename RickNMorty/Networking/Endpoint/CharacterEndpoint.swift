086084857cc33875259ef801260dd32d80ae69ac
Cem Telliagaoglu <cem.telliagaoglu@gmail.com>
2023-08-07 15:23:52 +0300

    Create CharacterEndpoint

---
+++ RickNMorty/Networking/Endpoint/CharacterEndpoint.swift
@@ -0,0 +1,40 @@
+//
+//  CharacterEndpoint.swift
+//  RickNMorty
+//
+//  Created by admin on 7.08.2023.
+//
+
+import Foundation
+
+public enum CharacterEndpoint {
+    case all
+    case single(Int)
+}
+
+extension CharacterEndpoint: Endpoint {
+    public var path: String {
+        switch self {
+        case .all:
+            return "/api/character"
+        case let .single(id):
+            return "/api/character/\(id)"
+        }
+    }
+
+    public var method: RequestMethod {
+        .get
+    }
+
+    public var header: [String : String]? {
+        nil
+    }
+
+    public var body: [String : String]? {
+        nil
+    }
+
+    public var queryItems: [URLQueryItem]? {
+        nil
+    }
+}
