263fce1472f7a8978b65be297a4b691f4ef01e4d
Cem Telliagaoglu <cem.telliagaoglu@gmail.com>
2023-08-07 15:37:47 +0300

    Add loadData function and handle request results

--- RickNMorty/Home/HomeInteractor.swift
+++ RickNMorty/Home/HomeInteractor.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 protocol HomeBusinessLogic: AnyObject {
-    
+    func loadData()
 }
 
 protocol HomeDataStore: AnyObject {
@@ -19,5 +19,18 @@ final class HomeInteractor: HomeBusinessLogic, HomeDataStore {
     
     var presenter: HomePresentationLogic?
     var worker: HomeWorkingLogic = HomeWorker()
+
+    func loadData() {
+        // TODO: check if exists in coredata
+        // if not existing, request all characters
+        worker.sendRequestForAllCharacters { [weak self] result in
+            switch result {
+            case .success(let response):
+                self?.presenter?.presentCharacters(model: response)
+            case .failure(let error):
+                self?.presenter?.presentError(message: error.customMessage)
+            }
+        }
+    }
     
 }
