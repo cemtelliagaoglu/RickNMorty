e1c87511e9b56bec2f6b57c8c485bb8b7208ab5c
Cem Telliagaoglu <cem.telliagaoglu@gmail.com>
2023-08-07 17:10:49 +0300

    Create UIImageView+Extension for downloading

---
+++ RickNMorty/UIImageView+Extension.swift
@@ -0,0 +1,27 @@
+//
+//  UIImageView+Extension.swift
+//  RickNMorty
+//
+//  Created by admin on 7.08.2023.
+//
+
+import UIKit
+
+extension UIImageView {
+    func downloadImage(from urlString: String, completion: @escaping(() -> Void)) {
+        guard let url = URL(string: urlString) else { return }
+        URLSession.shared.dataTask(with: url, completionHandler: ({ data, _, error in
+            if let error {
+                print(error)
+                completion()
+            } else {
+                if let imageData = data {
+                    DispatchQueue.main.async { [weak self] in
+                        self?.image = UIImage(data: imageData) ?? UIImage(systemName: "person.fill")
+                        completion()
+                    }
+                }
+            }
+        })).resume()
+    }
+}
