f83778cd0c35f5296ca758d31bd7f1bc1d045ff8
Cem Telliagaoglu <cem.telliagaoglu@gmail.com>
2023-08-07 14:04:28 +0300

    Add Request Method to networking

---
+++ RickNMorty/Networking/RequestMethod.swift
@@ -0,0 +1,16 @@
+//
+//  RequestMethod.swift
+//  RickNMorty
+//
+//  Created by admin on 7.08.2023.
+//
+
+import Foundation
+
+public enum RequestMethod: String {
+    case delete = "DELETE"
+    case get = "GET"
+    case patch = "PATCH"
+    case post = "POST"
+    case put = "PUT"
+}
