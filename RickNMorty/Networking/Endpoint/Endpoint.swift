782a56dbe6702b19158350c3beefe761b663b95e
Cem Telliagaoglu <cem.telliagaoglu@gmail.com>
2023-08-07 14:27:34 +0300

    Add Endpoint to networking

---
+++ RickNMorty/Networking/Endpoint/Endpoint.swift
@@ -0,0 +1,29 @@
+//
+//  Endpoint.swift
+//  RickNMorty
+//
+//  Created by admin on 7.08.2023.
+//
+
+import Foundation
+
+public protocol Endpoint {
+    var scheme: String { get }
+    var host: String { get }
+    var path: String { get }
+    var port: Int { get }
+    var method: RequestMethod { get }
+    var header: [String: String]? { get }
+    var body: [String: String]? { get }
+    var queryItems: [URLQueryItem]? { get }
+}
+
+public extension Endpoint {
+    var scheme: String {
+        "https"
+    }
+
+    var host: String {
+        "rickandmortyapi.com"
+    }
+}
