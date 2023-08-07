e71e2afece0744543ab29d35b80bbdaee68082fb
Cem Telliagaoglu <cem.telliagaoglu@gmail.com>
2023-08-07 13:25:38 +0300

    Initial Commit

---
+++ RickNMorty/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RickNMorty
+//
+//  Created by admin on 7.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
