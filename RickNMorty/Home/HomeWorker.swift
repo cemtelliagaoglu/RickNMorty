fed7c8b49ec6c3ddd2989cd2c5379146294a5bd7
Cem Telliagaoglu <cem.telliagaoglu@gmail.com>
2023-08-07 15:37:22 +0300

    Add allCharacters request

--- RickNMorty/Home/HomeWorker.swift
+++ RickNMorty/Home/HomeWorker.swift
@@ -8,9 +8,13 @@
 import Foundation
 
 protocol HomeWorkingLogic: AnyObject {
-    
+
+    func sendRequestForAllCharacters(completion: @escaping ((Result<Home.Case.Response, RequestError>) -> Void))
 }
 
-final class HomeWorker: HomeWorkingLogic {
-    
+final class HomeWorker: HomeWorkingLogic, HTTPClient {
+
+    func sendRequestForAllCharacters(completion: @escaping ((Result<Home.Case.Response, RequestError>) -> Void)) {
+        sendRequest(endpoint: CharacterEndpoint.all, responseModel: Home.Case.Response.self, completion: completion)
+    }
 }
