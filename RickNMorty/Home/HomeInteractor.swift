97cabf7a017113aaa6a99783b4e064c132d533c9
Cem Telliagaoglu <cem.telliagaoglu@gmail.com>
2023-08-08 11:07:06 +0300

    Add allCharacters property to HomeDataStore

--- RickNMorty/Home/HomeInteractor.swift
+++ RickNMorty/Home/HomeInteractor.swift
@@ -11,11 +11,14 @@ protocol HomeBusinessLogic: AnyObject {
     func loadData()
 }
 
-protocol HomeDataStore: AnyObject {}
+protocol HomeDataStore: AnyObject {
+    var allCharacters: [Home.Case.Response.Result]? { get }
+}
 
 final class HomeInteractor: HomeBusinessLogic, HomeDataStore {
     var presenter: HomePresentationLogic?
     var worker: HomeWorkingLogic = HomeWorker()
+    var allCharacters: [Home.Case.Response.Result]?
 
     func loadData() {
         // TODO: check if exists in coredata
@@ -23,6 +26,7 @@ final class HomeInteractor: HomeBusinessLogic, HomeDataStore {
         worker.sendRequestForAllCharacters { [weak self] result in
             switch result {
             case let .success(response):
+                self?.allCharacters = response.results
                 self?.presenter?.presentCharacters(model: response)
             case let .failure(error):
                 self?.presenter?.presentError(message: error.customMessage)
