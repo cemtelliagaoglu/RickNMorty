6c56d002719f7583daf2bed690a5a75bf7b521ea
Cem Telliagaoglu <cem.telliagaoglu@gmail.com>
2023-08-08 11:07:50 +0300

    Add routingDetails method

--- RickNMorty/Home/HomeRouter.swift
+++ RickNMorty/Home/HomeRouter.swift
@@ -5,15 +5,27 @@
 //  Created by admin on 7.08.2023.
 //
 
-import Foundation
+import UIKit
 
-protocol HomeRoutingLogic: AnyObject {}
+protocol HomeRoutingLogic: AnyObject {
+    func routeToCharacterDetails(at index: Int)
+}
 
-protocol HomeDataPassing: class {
+protocol HomeDataPassing: AnyObject {
     var dataStore: HomeDataStore? { get }
 }
 
 final class HomeRouter: HomeRoutingLogic, HomeDataPassing {
     weak var viewController: HomeViewController?
     var dataStore: HomeDataStore?
+
+    func routeToCharacterDetails(at index: Int) {
+        guard let character = dataStore?.allCharacters?[index] else { return }
+        let storyboard = UIStoryboard(name: "Details", bundle: nil)
+        guard let destinationVC = storyboard.instantiateViewController(withIdentifier: "DetailsViewController") as? DetailsViewController else { return }
+        destinationVC.setCharacter(id: character.id)
+        DispatchQueue.main.async { [weak self] in
+            self?.viewController?.navigationController?.pushViewController(destinationVC, animated: true)
+        }
+    }
 }
