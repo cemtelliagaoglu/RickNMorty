a7c5dd026b44ae064061e5bcaa5e6daa506e7d1c
Cem Telliagaoglu <cem.telliagaoglu@gmail.com>
2023-08-08 10:13:52 +0300

    Add loadCharacter method and handle results

--- RickNMorty/Details/DetailsInteractor.swift
+++ RickNMorty/Details/DetailsInteractor.swift
@@ -7,11 +7,24 @@
 
 import Foundation
 
-protocol DetailsBusinessLogic: AnyObject {}
+protocol DetailsBusinessLogic: AnyObject {
+    func loadCharacter(id: Int)
+}
 
 protocol DetailsDataStore: AnyObject {}
 
 final class DetailsInteractor: DetailsBusinessLogic, DetailsDataStore {
     var presenter: DetailsPresentationLogic?
     var worker: DetailsWorkingLogic = DetailsWorker()
+
+    func loadCharacter(id: Int) {
+        worker.sendRequestToCharacter(id: id) { [weak self] result in
+            switch result {
+            case let .success(response):
+                self?.presenter?.presentCharacter(model: response)
+            case let .failure(error):
+                self?.presenter?.presentError(error: error)
+            }
+        }
+    }
 }
