a8b5e6917a2e41b0488570d90832f77a6c55ae5d
Cem Telliagaoglu <cem.telliagaoglu@gmail.com>
2023-08-08 16:28:43 +0300

    Add CoreData and Character Entity

---
+++ RickNMorty/Character+CoreDataClass.swift
@@ -0,0 +1,11 @@
+//
+//  Character+CoreDataClass.swift
+//  RickNMorty
+//
+//  Created by admin on 8.08.2023.
+//
+
+import CoreData
+
+@objc(Character) public class Character: NSManagedObject {}
+
